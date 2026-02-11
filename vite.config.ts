5865495299270c970bc0cba03b19859ce426c771
raulmpsousa <raulmpsousa@gmail.com>
2026-02-11 18:40:44 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+// FIX: Import 'process' to provide correct types for process.cwd().
+import process from 'process';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.VITE_API_KEY)
+    }
+  }
+})
